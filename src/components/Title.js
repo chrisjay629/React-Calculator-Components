cd68fe013ddde473a8e62dc183474ed7f2c9a86a
chris jauregui <chrisjay629@gmail.com>
2017-04-17 18:41:19 -0500

    add components

---
+++ src/components/Title.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Title extends Component {
+  render (){
+     return(
+       <p>GA Instruments TI-WDI</p>
+
+     )
+
+  }
+}
+
+export default Title;
